1be5eaa6e054dbf000fea8d6d6f04e3709191a1f
Nic Luciano <nic.luciano@gmail.com>
2023-01-22 10:33:03 +0000

    prepping koa client

---
+++ examples/koa/src/client/index.ts
@@ -0,0 +1,16 @@
+import rpc from "@powerpc/client";
+
+import welcomeMessage from "../api/welcomeMessage";
+
+document.write("loading...");
+
+rpc(welcomeMessage)({
+  method: "GET",
+  query: { name: "foobar" },
+}).then((c) =>
+  document.write(
+    c._tag === "RemoteSuccess"
+      ? `Success: ${JSON.stringify(c.value)}`
+      : `Failure ${JSON.stringify(c.error)}`
+  )
+);
--- examples/koa/src/index.ts
+++ examples/koa/src/index.ts
@@ -6,6 +6,12 @@ import welcomeMessage from "./api/welcomeMessage";
 const app = new Koa();
 const router = new Router();
 
+router.get("/", (ctx) => {
+  ctx.type = "html";
+  ctx.body =
+    '<html><body><script type="text/javascript" src="foo.js" /></body></html>';
+});
+
 router.all("/welcomeMessage", welcomeMessage);
 
 app.use(router.routes()).listen(3000);
