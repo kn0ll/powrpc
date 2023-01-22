075d5b3d1c8f913a01346d741acf74da2a7f5f21
Nic Luciano <nic.luciano@gmail.com>
2023-01-22 10:08:14 +0000

    koa routing

--- examples/koa/src/index.ts
+++ examples/koa/src/index.ts
@@ -1,7 +1,11 @@
+import * as Router from "@koa/router";
 import * as Koa from "koa";
 
 import welcomeMessage from "./api/welcomeMessage";
 
 const app = new Koa();
+const router = new Router();
 
-app.use(welcomeMessage).listen(3000);
+router.all("/welcomeMessage", welcomeMessage);
+
+app.use(router.routes()).listen(3000);
