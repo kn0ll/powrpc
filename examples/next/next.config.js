413ffeaa7dbe6b8ad5011a9a852195afdc60a970
Nic Luciano <nic.luciano@gmail.com>
2023-01-22 09:13:07 +0000

    simpler plugin

--- examples/next/next.config.js
+++ examples/next/next.config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { nextPlugin } = require("@powerpc/server-next");
 
-module.exports = nextPlugin(/pages\/api\/.*/)({});
+module.exports = nextPlugin({});
--- packages/server-next/src/plugin.ts
+++ packages/server-next/src/plugin.ts
@@ -1,9 +1,9 @@
-export const nextPlugin = (test: RegExp) => (nextConfig: any) =>
+export const nextPlugin = (nextConfig: any) =>
   Object.assign({}, nextConfig, {
     webpack(config: any, options: any) {
       if (!options.isServer) {
         config.module.rules.push({
-          test,
+          test: /pages\/api\/.*/,
           use: {
             loader: "@powerpc/webpack-loader",
             options: { apiUrl: (path: string) => `/api${path}` },
